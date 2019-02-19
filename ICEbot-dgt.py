5b463d4342a002ea6d5136e908536e3d23c5e371
Sergio <scaverod@gmail.com>
2019-02-19 17:42:41 +0100

    path updated

--- ICEbot-dgt.py
+++ ICEbot-dgt.py
@@ -1,9 +1,18 @@
 import argparse
-
+import platform
 from selenium import webdriver
 
-PATH_CHROMEDRIVER = "YOUR_PATH_HERE"
+PATH_CHROMEDRIVER = ""
 OPTIONS = ['headless']
+
+if PATH_CHROMEDRIVER == "":
+    platform = platform.system()
+    if platform == 'Linux':
+        PATH_CHROMEDRIVER = "chromedriver_linux64/chromedriver"
+    elif platform == 'Windows':
+        PATH_CHROMEDRIVER = "chromedriver_win32\\chromedriver.exe"
+    elif platform == 'Darwin':
+        PATH_CHROMEDRIVER = "chromedriver_mac64/chromedriver"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path-chromedriver", help="Path de la localización de chromedriver", type=str,
@@ -44,7 +53,7 @@ def get_driver(path, options_arg):
         print("Invalid driver path. \nDownload driver here: "
               "https://chromedriver.storage.googleapis.com/index.html?path=73.0.3683.20/")
         raise e
-    print("Driver path is correct... starting scrapping...")
+    print("Driver path is correct... program is ready...")
     return driver
 
 
@@ -56,12 +65,18 @@ if __name__ == "__main__":
         if args.carretera.upper() != "ALL":
             print("Se van a buscar incidentes para la carretera ", args.carretera.upper())
         else:
+            print("################################################################################################")
+            print("#### PARA BUSCAR POR UNA CARRETERA EN CONCRETO USAR python ICEbot-dgt.py -c <carretera>  #######")
+            print("################################################################################################")
             print("Se van a buscar todos los incidentes de las carreteras españolas")
+            input("Presione la tecla enter....")
         data = get_incidencias(driver, args.carretera.upper())
         if data is not None:
             print("Se han encontrado " + str(len(data)) + " incidencias.")
             for incident in data:
                 print(incident)
+
+            print("\n Se han encontrado " + str(len(data)) + " incidencias.")
         else:
             print("No se han encontrado incidencias")
     except Exception:
